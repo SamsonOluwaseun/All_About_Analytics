1b56e52718f5189d32bd97bb5b9d80140546d1a6
SamsonOluwaseun <39919583+SamsonOluwaseun@users.noreply.github.com>
2024-04-23 23:11:31 +0000

    My Python script

---
+++ Python/Data Engineer with Python/main.py
@@ -0,0 +1,13 @@
+string= "Hello World!"
+integer = 4
+float_ = 3.14
+float_2= float(4)
+integer_2= int(3.94)
+boolean= True
+boolean_2= 3==2
+operations= 3+4
+operations_2= 12/ 4
+operations_3= 12// 4
+operation_4 = int("12")/4
+variable_of_interest= operation_4
+print(variable_of_interest, type(variable_of_interest))
